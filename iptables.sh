9b1299bac0b0f1f3e31bb3fa47703f735c3c5736
liuhuahang <liuhuahang@zerus.co>
2014-09-19 21:51:15 +0800

    add v1.4.7
    N/A
    
    Signed-off-by: liuhuahang <liuhuahang@zerus.co>

---
+++ iptables.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+iptables -t nat -N SHADOWSOCKS
+iptables -t nat -A SHADOWSOCKS -d 106.186.123.206 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 0.0.0.0/8 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 10.0.0.0/8 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 127.0.0.0/8 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 169.254.0.0/16 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 172.16.0.0/12 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 192.168.0.0/16 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 224.0.0.0/4 -j RETURN
+iptables -t nat -A SHADOWSOCKS -d 240.0.0.0/4 -j RETURN
+iptables -t nat -A SHADOWSOCKS -p tcp -j REDIRECT --to-ports 12345
+iptables -t nat -A PREROUTING -p tcp -j SHADOWSOCKS
---
+++ r1cm/start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+./ss-redir -c config.json -f /tmp/ss-redir.log
